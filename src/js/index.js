8ba9f3a0235bd2e6b940298a0d2db0bd6133bfa5
Alex <little_al14@hotmail.com>
2017-04-30 14:06:42 -0300

    basic ui added

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,51 @@
 var Module = {};
 Module.onRuntimeInitialized = ()=> {
-  Module.ccall('createMineField','undefined', ['number', 'number', 'number'], [8,8,64]);
-  Module.ccall('revealLocation','number', ['number', 'number'], [0,0]);
+  const createMineField = Module.cwrap('createMineField','undefined', ['number', 'number', 'number']);
+  const revealLocation = Module.cwrap('revealLocation','number', ['number', 'number']);
+  const viewBoard = Module.cwrap('viewBoard','string', null);
 
-  const board = Module.ccall('viewBoard','string', null, null);
-
-  console.log(board);
+  const createFieldSection = document.querySelector('.createfield');
+  const createFieldBtn = document.querySelector(".createfield > button");
+  createFieldBtn.addEventListener("click", ()=> {
+    let inputs = document.querySelectorAll(".createfield input");
+    let height = inputs[0].value;
+    let width = inputs[1].value;
+    let minecount = inputs[2].value;
+    createMineField(height, width, minecount);
+    createFieldSection.style.display = "none";
+    let board = viewBoard();
+    createBoard(height, width, minecount, board);
+  });
 };
+
+const createBoard = (height, width, minecount, board) => {
+  let mineArray = splitBoardStr(board);
+  console.log(mineArray);
+  let docfragment = document.createDocumentFragment();
+  for (let h = 0; h < height; h++) {
+    let row = document.createElement("tr");
+    for (let w = 0; w < width; w++) {
+      let cell = document.createElement("td");
+      let mine = mineArray[w+h*width];
+      console.log(mine);
+      if (mine[1] === "1") {
+        console.log("mine is revealed");
+      }
+      row.appendChild(cell);
+    }
+    docfragment.appendChild(row);
+  }
+  let table = document.querySelector('table');
+  table.appendChild(docfragment);
+}
+
+const splitBoardStr = (board)=>{
+  let mineArray = [];
+  let currBoard = board;
+  do {
+    let mine = currBoard.substr(0,2);
+    currBoard = currBoard.substr(2);
+    mineArray.push(mine);
+  } while (currBoard !== "");
+  return mineArray;
+}
